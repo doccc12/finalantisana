3409a628e4a888c2c98cdf9bd72026eeb9440a10
elkinyau <elkinn2n@gmail.com>
2026-02-10 22:22:45 -0500

    final

--- script.js
+++ script.js
@@ -32,3 +32,31 @@ document.querySelectorAll('.acordeon-header').forEach(header => {
         item.classList.toggle('active');
     });
 });
+
+function buscarEnTabla() {
+    var input = document.getElementById("inputBuscador");
+    var filtro = input.value.toUpperCase();
+    var tabla = document.getElementById("tablaDiccionario");
+    var filas = tabla.getElementsByTagName("tr");
+
+    for (var i = 1; i < filas.length; i++) {
+        var celdaNombreOriginal = filas[i].getElementsByTagName("td")[0]; 
+        var celdaNombre = filas[i].getElementsByTagName("td")[1];         
+
+        if (celdaNombreOriginal && celdaNombre) {
+            var texto1 = celdaNombreOriginal.textContent || celdaNombreOriginal.innerText;
+            var texto2 = celdaNombre.textContent || celdaNombre.innerText;
+
+            if (texto1.toUpperCase().indexOf(filtro) > -1 || texto2.toUpperCase().indexOf(filtro) > -1) {
+                filas[i].style.display = "";
+            } else {
+                filas[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function cerrarModal() {
+    var modal = document.getElementById("modal-advertencia");
+    modal.style.display = "none";
+}
